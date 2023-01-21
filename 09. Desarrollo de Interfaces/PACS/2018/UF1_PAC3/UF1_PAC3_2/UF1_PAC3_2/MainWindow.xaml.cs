2f1a7a0d6417162a35885c17d14370c1b4083611
ansango <anibalsantosgo@gmail.com>
2023-01-21 19:04:30 +0100

    interfaces

---
+++ 09. Desarrollo de Interfaces/PACS/2018/UF1_PAC3/UF1_PAC3_2/UF1_PAC3_2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UF1_PAC3_2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void calcu_click(object sender, EventArgs evento)
+        {
+            if (this.centi.Text.Length > 0)
+            {
+                float centigradosG = float.Parse(this.centi.Text);
+                float farenheitG = (centigradosG * 9 / 5) + 32;
+                this.faren.Text = farenheitG.ToString();
+            }
+            else if (this.faren.Text.Length > 0)
+            {
+                float farenheitG = float.Parse(this.faren.Text);
+                float centigradosG = (farenheitG - 32) * 5 / 9;
+                this.centi.Text = centigradosG.ToString();
+            }
+            else
+            {
+                this.centi.Text = "0";
+                calcu_click(sender, evento);
+            }
+        }
+    }
+}
---
+++ 09. Desarrollo de Interfaces/PACS/2018/UF1_PAC3/UF1_PAC3_Anibal_Santos_Gomez/UF1_PAC3_3/UF1_PAC3_3/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UF1_PAC3_3
+{
+    public partial class MainWindow : Window
+    {
+        private float dinero;
+
+        public event EventHandler dineroCambiado;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected virtual void dineroCambia() //clase virtual vacia
+        {
+            if (dineroCambiado != null) //
+            {
+                dineroCambiado(this, EventArgs.Empty);
+            }
+            else
+            {
+                if (Dinero < 0)
+                {
+                    MessageBox.Show("No tienes saldo suficiente en tu haber: " + Dinero.ToString(), "Advertencia // Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+        public float Dinero // getters y setters
+        {
+            get
+            {
+                return dinero; 
+            }
+
+            set
+            {
+                dinero = value;
+                dineroCambia();
+            }
+        }
+
+        // Botones de eventos
+        private void ingresarClick(object sender, RoutedEventArgs evento)
+        {
+            float cant = float.Parse(this.cantidad.Text);
+            this.cantidad.Text = "";
+            Dinero = Dinero + cant;
+        }
+        private void retirarClick(object sender, RoutedEventArgs evento)
+        {
+            float cant = float.Parse(this.cantidad.Text);
+            this.cantidad.Text = "";
+            Dinero = Dinero - cant;
+        }
+    }
+}
